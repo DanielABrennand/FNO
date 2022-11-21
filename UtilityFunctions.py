cba2d0a7187ed5db82d03bc9c014f4316e0994f8
DBren <danielabrennand@outlook.com>
2022-11-21 10:33:03 +0000

    Added Lp Loss class

--- UtilityFunctions.py
+++ UtilityFunctions.py
@@ -1,6 +1,7 @@
 import numpy as np
 import h5py
 from PIL import Image
+from torch import norm, mean, sum
 class LogitPercentConverter:
     #Takes a logit and returns the corresponding probability (0 to 1) or vice versa
     @staticmethod
@@ -31,3 +32,48 @@ class H5ToNumpy:
                     img = Image.fromarray(img).resize((Resolution,Resolution))
                 Data[:,:,n] = img
         return Data
+
+class LpLoss(object):
+    def __init__(self, d=2, p=2, size_average=True, reduction=True):
+        super(LpLoss, self).__init__()
+
+        #Dimension and Lp-norm type are postive
+        assert d > 0 and p > 0
+
+        self.d = d
+        self.p = p
+        self.reduction = reduction
+        self.size_average = size_average
+
+    def abs(self, x, y):
+        num_examples = x.size()[0]
+
+        #Assume uniform mesh
+        h = 1.0 / (x.size()[1] - 1.0)
+
+        all_norms = (h**(self.d/self.p))*norm(x.view(num_examples,-1) - y.view(num_examples,-1), self.p, 1)
+
+        if self.reduction:
+            if self.size_average:
+                return mean(all_norms)
+            else:
+                return sum(all_norms)
+
+        return all_norms
+
+    def rel(self, x, y):
+        num_examples = x.size()[0]
+
+        diff_norms = norm(x.reshape(num_examples,-1) - y.reshape(num_examples,-1), self.p, 1)
+        y_norms = norm(y.reshape(num_examples,-1), self.p, 1)
+
+        if self.reduction:
+            if self.size_average:
+                return mean(diff_norms/y_norms)
+            else:
+                return sum(diff_norms/y_norms)
+
+        return diff_norms/y_norms
+
+    def __call__(self, x, y):
+        return self.rel(x, y)
